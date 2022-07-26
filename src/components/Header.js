b0ef2148fc83953e0226645e8741d7baac5f1838
Juan Pablo Lujan <juampa_123@hotmail.com>
2022-07-26 20:04:03 -0300

    component NavBar y descarga de libreria  estilos sass

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import NavBar from "./NavBar"
+const Header=()=>{
+    return(
+        <header id="header">
+            <NavBar />
+        </header>
+    )
+}
+export default Header;
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+const  NavBar = () => {
+    return (
+        <>
+            <nav>
+                <div id="logo_Nav">
+                    <img src="../logo192.png" alt="logo de Charme" srcset="" />
+                </div>
+                <div id="titulo_Nav">
+                    <h1 classname= "titulo" >Charme Calzados</h1>
+                </div>
+                <div id="link_Nav">
+                    <a href="#"> link </a>
+                    <a href="#"> link </a>
+                    <a href="#"> link </a>
+                </div>
+            </nav>     
+        </>
+    )
+}
+
+export default NavBar;
